aeaafacfc470f082c22d93ff5b9b127cf1ec0b1a
shigeno shinji <sazen.shinji@gmail.com>
2026-01-07 14:18:35 +0900

    2026.01.07 14.18 Update

---
+++ src/resources/views/top_page.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('css//top_page.css') }}">
+@endsection
+
+@section('content')
+
+<div class="topp__content">
+  <h1>ブラウザに画面を表示するだけ。</h1>
+</div>
+
+@endsection
